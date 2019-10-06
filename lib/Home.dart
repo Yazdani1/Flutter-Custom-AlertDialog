109532f40f3f2be173f52419e78cb3542db9f05b
Yazdani <yaz4dani@gmail.com>
2019-10-06 14:17:47 +0600

    Custom dialog

--- lib/Home.dart
+++ lib/Home.dart
@@ -128,6 +128,7 @@ class _HomeState extends State<Home> {
                               }
                           ),
                           ),
+                        
                         new SizedBox(width: 6.0,),
                         new Expanded(
                           flex: 1,
